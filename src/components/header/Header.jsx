da30d56f984168b157b8498043c1764575be61dd
Irina Baeva <irina.baeva@bonial.com>
2020-09-03 13:17:21 +0200

    Set up of the web up

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './header.scss'
+export default function Header(){
+
+    return (
+        <header className="header">
+            <div className="header__inner">
+                <a className="logo" href="/">
+                    <img src="" alt="" className="logo__image" />
+                    <div>Domestic Violence App</div>
+                </a>
+                <div className="header__menu">
+                    Menu
+          </div>
+            </div>
+
+        </header>
+    );
+}
