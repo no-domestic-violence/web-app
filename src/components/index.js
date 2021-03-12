319c8ace62b908ac33c342c024e50f6878f09c78
soyoon choi <soyoon.choi@code.berlin>
2021-03-12 13:10:27 +0100

    move protectedroute to hoc

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 import Header from './header/Header';
-import ProtectedRoute from './protected-route/ProtectedRoute';
+import ProtectedRoute from './hoc/protected-route/ProtectedRoute';
 import LoginForm from './login-form/LoginForm';
 
 export { Header, ProtectedRoute, LoginForm };
