db08fce31af85f8747a44d5f1efb1213d4cbadcf
BehnazDer <behnaz6788@gmail.com>
2021-08-02 23:36:24 +0200

    switched to Netlify

--- src/config/index.js
+++ src/config/index.js
@@ -3,10 +3,10 @@ let baseURL, captchaSiteKey;
 
 if (process.env.NODE_ENV === 'development') {
   baseURL = 'http://localhost:3001/api';
-  captchaSiteKey = '6Lfb_88bAAAAAOKvnf55nxS2eAKFxlkHym-fRfiD';
+  captchaSiteKey = '6Lfbl9YbAAAAAMk1f2oxSzqsuapsrCfLp9S_LuPs';
 } else {
   baseURL = 'https://pool-api-mobile.herokuapp.com/api';
-  captchaSiteKey = '6Lfb_88bAAAAAOKvnf55nxS2eAKFxlkHym-fRfiD';
+  captchaSiteKey = '6Lfbl9YbAAAAAMk1f2oxSzqsuapsrCfLp9S_LuPs';
 }
 
 export { baseURL, captchaSiteKey };
